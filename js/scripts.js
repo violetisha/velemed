0a821302542fafadd9659b49fcaab7ffc18a2683
Violeta Rosales <ellaes@violetarosales.com>
2013-07-09 07:15:49 -0500

    Fuente corrupta. Boton arriba

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,25 @@ $(document).ready(function(){
 	$('#slider').nivoSlider( {
 		effect:'fade'
 	});
+
+	
+	$("#top").hide();
+	
+
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			$('#top').fadeIn();
+		} else {
+			$('#top').fadeOut();
+		}
+	});
+
+	// scroll body to 0px on click
+	$('#top a').click(function () {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 800);
+		return false;
+	});
+
 });
