07a461e8689635d463abdde9b9238b9159d69c61
Violeta Rosales <ellaes@violetarosales.com>
2013-07-09 09:01:56 -0500

    Slider

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,54 @@
 $(document).ready(function(){
-	
-	$('#tabs').easytabs();
-	
-	$('#slider').nivoSlider( {
-		effect:'fade'
-	});
+	// tabs
+	$("#tabs").easytabs();
 
-	
+	//top
 	$("#top").hide();
-	
-
 	$(window).scroll(function () {
 		if ($(this).scrollTop() > 100) {
-			$('#top').fadeIn();
+			$("#top").fadeIn();
 		} else {
-			$('#top').fadeOut();
+			$("#top").fadeOut();
 		}
 	});
-
-	// scroll body to 0px on click
-	$('#top a').click(function () {
-		$('body,html').animate({
+	$("#top a").click(function () {
+		$("body,html").animate({
 			scrollTop: 0
 		}, 800);
 		return false;
 	});
 
+	//lightbox
+	$(".fancybox-media").fancybox({
+		arrows : false,
+		helpers : {
+			media : {}
+		}
+	});
+
+	//slider
+	$('#slider').slidesjs({
+        width: 940,
+        height: 528,
+        navigation: {
+        	active: "false",
+        	effect: "fade"
+        },
+        pagination: {
+        	active: "false",
+        	effect: "fade"
+        },
+        effect: {
+        	fade: {
+            	speed: 400
+        	}
+        },
+        play: {
+	        active: true,
+	        auto: true,
+	        pauseOnHover: false,
+	        effect: "fade",
+	        interval: 3000
+	    }
+      });
 });
