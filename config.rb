37799e1c858a40286d5de23fbfe3f17901146a7f
Violeta Rosales <ellaes@violetarosales.com>
2013-07-13 17:55:19 -0500

    Nueva estructura de directorios

--- config.rb
+++ config.rb
@@ -4,17 +4,17 @@
 http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
-images_dir = "img"
-javascripts_dir = "js"
+images_dir = "imagenes"
+javascripts_dir = "libreria/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded # or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 preferred_syntax = :sass
 require 'sassy-buttons'
